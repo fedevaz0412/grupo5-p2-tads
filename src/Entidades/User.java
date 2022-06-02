0a74d8522db2cce155dc5a0bf1925f7b30568570
Federico Vazquez <federicovazquez@Air-de-Federico.lan>
2022-06-01 21:08:25 -0300

    Agregar entidades User, Review

---
+++ src/Entidades/User.java
@@ -0,0 +1,14 @@
+package Entidades;
+
+public class User {
+    //Atributos
+    private String username;
+    //Constructor
+    public String getUsername() {
+        return username;
+    }
+    //Getters
+    public User(String username){
+        this.username = username;
+    }
+}
