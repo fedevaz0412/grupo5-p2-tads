50807135fbc91059b9671157f0475382d42c3e51
federicovazquez <favazquez@correo.um.edu.uy>
2022-06-07 15:43:21 -0300

    Agregar Review -> beerId

--- src/Entidades/Review.java
+++ src/Entidades/Review.java
@@ -11,14 +11,18 @@ public class Review {
     private double aromaScore;
     private double appearanceScore;
     private double flavourScore;
+    private long beerId;
+
     //Constructor
-    public Review(long id, Date date, double overallScore, double aromaScore, double appearanceScore, double flavourScore) {
+    public Review(long id, Date date, double overallScore, double aromaScore, double appearanceScore, double flavourScore, long beerId) {
         this.id = id;
         this.date = date;
         this.overallScore = overallScore;
         this.aromaScore = aromaScore;
         this.appearanceScore = appearanceScore;
         this.flavourScore = flavourScore;
+        this.beerId = beerId;
+
     }
     //Getters
     public long getId() {
