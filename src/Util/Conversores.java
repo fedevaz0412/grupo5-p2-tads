d4f44a3597735e7dd99d4d55536f4700b59b5dd3
Clara <cecheverria@correo.um.edu.uy>
2022-06-11 17:27:33 -0300

    agregar nuevas conversiones

--- src/Util/Conversores.java
+++ src/Util/Conversores.java
@@ -2,6 +2,8 @@ package Util;
 
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
 import java.util.Date;
 
 public class Conversores {
@@ -10,7 +12,7 @@ public class Conversores {
         Date d = new Date(Long.parseLong(date) * 1000);
         SimpleDateFormat format = new SimpleDateFormat("dd/MM/yyyy");
         String formatted = format.format(d);
-        System.out.println(formatted);
+        //System.out.println(formatted);
         Date dateParaRetornar = null;
         try {
             dateParaRetornar = format.parse(formatted);
@@ -18,5 +20,29 @@ public class Conversores {
             e.printStackTrace();
         }
         return dateParaRetornar;
+    }
+
+    public static LocalDateTime convertToLocalDateTimeViaInstant(Date dateToConvert) {
+        return dateToConvert.toInstant()
+                .atZone(ZoneId.systemDefault())
+                .toLocalDateTime();
+    }
+
+    public static Double stringADouble (String unString){
+        Double d = 0.0;
+
+        if(unString != "" && unString != null){
+            d = Double.parseDouble(unString);
+        }
+        return d;
+    }
+
+    public static Long stringALong (String unString){
+        Long l = Long.valueOf(0);
+
+        if(unString != "" && unString != null){
+            l = Long.parseLong(unString);
+        }
+        return l;
     }
 }
